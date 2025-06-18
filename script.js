81120823c5669c42acbae337de37b2a94c5889e9
syahrulazinar <145322564+syahrulazinar@users.noreply.github.com>
2025-06-18 13:25:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("Thank you for reaching out! We'll get back to you soon.");
+  this.reset();
+});
